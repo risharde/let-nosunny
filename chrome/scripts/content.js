460e17a50758679af183b1c172c4da4548611a28
Richard Leishman <richard@webfwd.co.uk>
2024-01-17 17:51:23 +0000

    Remove the need of jQuery.
    Vanilla JS Baby!

--- chrome/scripts/content.js
+++ chrome/scripts/content.js
@@ -1,5 +1,8 @@
-$(document).ready(function (){
+window.addEventListener('load', function() {
     // Go through each post
-    $(document).find('.Author a[title="yoursunny"]').parent().parent().parent().parent().parent().fadeOut(200);
-    $(document).find('.Author a[title="tototo"]').parent().parent().parent().parent().parent().fadeOut(200);
+    let elements = document.querySelectorAll('.Author a[title="yoursunny"],.Author a[title="tototo"]');
+    for (let i = 0; i < elements.length; i++) {
+        let item = elements[i].parentNode.parentNode.parentNode.parentNode.parentNode;
+        item.style.display = 'none';
+    }
 });
